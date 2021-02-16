888fdd3e8fe9d2ef5d73cff9901841bb38f35476
Yueh-Hua Tu <a504082002@gmail.com>
2021-02-16 22:24:22 +0800

    add CI

--- docs/make.jl
+++ docs/make.jl
@@ -6,9 +6,10 @@ makedocs(;
     authors="Yueh-Hua Tu",
     repo="https://github.com/yuehhua/SnowyOwl.jl/blob/{commit}{path}#L{line}",
     sitename="SnowyOwl.jl",
+    clean = false,
     format=Documenter.HTML(;
         prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://yuehhua.github.io/SnowyOwl.jl",
+        canonical="https://yuehhua.github.io/SnowyOwl.jl/stable/",
         assets=String[],
     ),
     pages=[
@@ -18,4 +19,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/yuehhua/SnowyOwl.jl",
+    target = "build",
 )
